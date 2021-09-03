ea89854d45b5e1601e839c631466fa127339f0e1
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2021-09-02 23:32:45 -0500

    Update wall_bouncer.py

--- wall_bouncer.py
+++ wall_bouncer.py
@@ -1,5 +1,10 @@
 """
+1. Enable the motors. Confirm the motors are enabled (check GPIO pins voltage level). 
+2. Robot entering **Pausing** mode. Set the GREEN LED to be a dimmer (PWM). Make sure robot's motion is stopped in this mode.
+3. Press the button to enable the **Playing** mode. Robot should be able to move forward. Light up GREEN LED and keep the brightness a constant. Use the distance sensor (Feel free to use more than one) to monitor walls in front of the robot. Turn the robot away from the approaching wall within a certain distance.
+4. Press the button again will be able to switch the mode back to **Pausing**. Pressing the button later on can switch the mode back and forth. 
 
+Record time consumption in **Playing** mode, once over 60 seconds, light up YELLOW LED. If over 90 seconds, blink (turn on then turn off) the RED LED 10 times, then shut down.
 """
 
 #### Write your code below ####
